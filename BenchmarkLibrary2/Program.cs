cc999496c3d2c34a2a5a9a1031c4bc47b70a5a45
elizalcodes <elizal.codes@gmail.com>
2023-07-13 15:16:56 +0200

    add new benchmark project

---
+++ BenchmarkLibrary2/Program.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+var config = ManualConfig.Create(DefaultConfig.Instance)
+    .WithOptions(ConfigOptions.StopOnFirstError);
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
